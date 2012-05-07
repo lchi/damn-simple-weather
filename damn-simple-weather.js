c157043dc3dbc0a039ce038da99cd390fc4d28a7
Lucas Chi <chi.lucas@gmail.com>
2012-05-06 23:38:30 -0400

    rendering views, crazy routing now :/

--- damn-simple-weather.js
+++ damn-simple-weather.js
@@ -4,76 +4,99 @@ var request = require('request');
 var xml_parser = require('libxml-to-js');
 
 var app = express.createServer();
+app.set("view engine", "ejs");
 
 var cache = {
   date: Date.today()
 };
 
 app.get('/', function(req, res){
-    res.send("Use /(zipcode) to get weather for your zipcode. ie: http://[myurl.com]/10027");
+    if(req.query.zip) {
+        get_weather(res, req.query.zip, render_weather);
+    } else {
+        res.render("index.ejs");
+    }
 });
 
 app.get('/:zip', function(req, res){
-    get_weather(req.params.zip, function (error, weather) {
-        if(Date.compare(Date.today(), cache.date) !== 0) {
-            cache = {
-              date: Date.today()
-            };
-        }
-        if(error) {
-            res.send(error);
-        } else {
-            if (cache['z' + req.params.zip] !== undefined) {
-                res.send(cache['z' + req.params.zip]);
-            } else {
-                reply = "Weather for: " + req.params.zip + " on " +
-                    Date.today().toFormat("YYYY-MM-DD") + "<br />" +
-                    weather.min + "-" + weather.max + "F<br />" +
-                    weather.precip + "% chance of precipitation";
-                reply = "<div style=\"text-align: center\">" + reply + "</div>";
-                cache['z' + req.params.zip] = reply;
-                res.send(reply);
-            }
-        }
-    });
+    get_weather(res, req.params.zip, render_weather);
 });
 
-function get_weather(zip, callback) {
+function render_weather(zip, res, error, weather) {
+    if(Date.compare(Date.today(), cache.date) !== 0) {
+        cache = {
+          date: Date.today()
+        };
+    }
+    if(error) {
+        res.send(error);
+    } else {
+        if (cache['z' + zip] !== undefined) {
+            res.send(cache['z' + zip]);
+        } else {
+            res.render("weather_for_zip", {
+              weather: weather,
+              zip: zip,
+              today: Date.today().toFormat("YYYY-MM-DD")
+            });
+        }
+    }
+}
+
+function get_weather(res, zip, callback) {
     request(weather_api_str(zip), function (error, response, body) {
         if(!error && response.statusCode == 200) {
             xml_parser(body, function (parser_error, result) {
                 if(parser_error) {
-                    callback("Parser error: " + parser_error);
+                    callback(zip, res, "Parser error: " + parser_error);
                 } else if(result.error) {
-                    callback("API error " + result.error);
+                    callback(zip, res, "API error " + result.error);
                 } else if(result) {
                     var result_params = result.data.parameters;
-                    var max_temp = parseInt(result_params.temperature[0].value);
-                    var min_temp = parseInt(result_params.temperature[1].value);
-                    var mean_temp = (max_temp + min_temp) / 2;
 
-                    var precip_prob = Math.max(result_params["probability-of-precipitation"].value[0]["#"],
-                        result_params["probability-of-precipitation"].value[1]["#"]);
+                    var precip_prob;
+                    // there may be a second precip prob
+                    if(!result_params["probability-of-precipitation"].value[1]){
+                        precip_prob = result_params["probability-of-precipitation"].value;
+                    } else {
+                        precip_prob = Math.max(result_params["probability-of-precipitation"].value[0]["#"],
+                            result_params["probability-of-precipitation"].value[1]["#"]);
+                    }
 
-                    callback(null, {
-                        max: max_temp,
-                        min: min_temp,
-                        mean: mean_temp,
-                        precip: precip_prob
-                    });
+                    var temp;
+                    // there is only one temperature, so its not an array
+                    if(!result_params.temperature[0]){
+                        temp = parseInt(result_params.temperature.value);
+
+                        callback(zip, res, null, {
+                            temp: temp,
+                            precip: precip_prob
+                        });
+                    } else {
+                        var max_temp = parseInt(result_params.temperature[0].value);
+                        var min_temp = parseInt(result_params.temperature[1].value);
+                        var mean_temp = (max_temp + min_temp) / 2;
+
+                        callback(zip, res, null, {
+                            max: max_temp,
+                            min: min_temp,
+                            mean: mean_temp,
+                            precip: precip_prob
+                        });
+                    }
                 } else {
-                  res.send("Zip code not found??");
+                  callback(zip, res, "Zip code not found??");
                 }
             });
         } else {
-            callback("There was an error" + error);
+            callback(zip, res, "There was an error" + error);
         }
     });
 
-};
+}
 
 function weather_api_str(zip) {
-    return "http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdXMLclient.php?zipCodeList=" + zip + "&product=time-series&begin=" + Date.today().toFormat("YYYY-MM-DD") + "&end=" + Date.tomorrow().toFormat("YYYY-MM-DD") + "&maxt=maxt&mint=mint&pop12=pop12"
+    return "http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdXMLclient.php?zipCodeList=" + zip + "&product=time-series&begin=" + Date.today().toFormat("YYYY-MM-DD") + "&end=" + Date.tomorrow().toFormat("YYYY-MM-DD") + "&maxt=maxt&mint=mint&pop12=pop12";
 }
 
 app.listen(process.env.PORT || 6767);
